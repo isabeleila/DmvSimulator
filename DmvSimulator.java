4ccf8d108e861f59df69412a8a87d7de6fc97c45
isabeleila <leilaiayers@gmail.com>
2025-02-26 19:35:40 -0500

    increased wait time

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -7,7 +7,7 @@ public class DmvSimulator{
         System.out.println("Hi, welcome to the DMV!");
         
         Random rand = new Random();
-        int randNum = rand.nextInt(100) +1;
+        int randNum = rand.nextInt(200) +1;
         System.out.println("Your number in line is " + randNum);
         
         int currentNum = randNum +1;
@@ -15,7 +15,7 @@ public class DmvSimulator{
         while (currentNum != randNum){
             System.out.println("Next up is: " + currentNum + "!");
 
-            if (currentNum == 100){
+            if (currentNum == 200){
                 currentNum = 1;
             } else{
                 currentNum++;
