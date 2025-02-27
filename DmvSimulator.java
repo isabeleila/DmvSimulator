64b8b9e571c17a38aada43ddfc0a87b184fb91bf
isabeleila <leilaiayers@gmail.com>
2025-02-27 11:08:08 -0500

    Adding chance of getting nice message

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -4,6 +4,9 @@ public class DmvSimulator{
     public static void main (String args[]){
         System.out.println("Hi, welcome to the DMV!");
         
+        Random chance =  new Random();
+        double messageChance = chance.nextDouble(1);
+
         Random rand = new Random();
         int randNum = rand.nextInt(200) +1;
         System.out.println("Your number in line is " + randNum);
@@ -19,7 +22,10 @@ public class DmvSimulator{
                 currentNum++;
             }
         } System.out.println("Your number has been called!");
-        System.out.println("You do not have the required paperwork, get out of the line and never return!");
-
+        if (chance.nextDouble() < 0.99){
+            System.out.println("You do not have the required paperwork, get out of the line and never return!");
+        } else{
+            System.out.println("You have all of the required paperwork! You're all set, have a nice day.");
+        }
     }
 }
